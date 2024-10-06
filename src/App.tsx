ddc7eb16faa00d18d153c6788bc8e529744c9da3
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-06 11:18:40 +0100

    change path

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <div>
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/store" element={<Store />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
