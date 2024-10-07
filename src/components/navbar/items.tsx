1bea77089f986edd86171f7af42b8191884cfed9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-07 20:07:27 +0100

    fix cartItem details and enhanc styling

--- src/components/navbar/items.tsx
+++ src/components/navbar/items.tsx
@@ -12,32 +12,35 @@ import {
 import { FaMinus } from "react-icons/fa6";
 
 const Items = () => {
-  const { cart,totalQuantityPrice } = useSelector((state: RootState) => state.cart);
+  const { cart, totalQuantityPrice } = useSelector(
+    (state: RootState) => state.cart
+  );
   const dispatch = useDispatch();
   useEffect(() => {
     console.log(cart);
   }, []);
   return (
     <>
-      <div className="overflow-scroll w-[450px] h-[350px] border-2 border-gray-200 rounded-md bg-white shadow-lg">
+      <div className="overflow-scroll w-[80vh] h-[350px] border-2 border-gray-200 rounded-md bg-white shadow-lg">
         <h1 className="font-bold text-4xl p-2 mx-2">Your Items</h1>
 
         <div className="grid grid-cols-1 gap-4">
           <div className="w-full h-full">
             {cart.map((pic, idx) => (
-              <div key={idx} className="flex m-2">
+              <div key={idx} className="flex m-4 shadow-md rounded-md ">
                 <img
-                  src={pic.product.url}
+                  src={pic.product.images}
                   alt="cart image"
-                  className="w-[120px] h-[120px] rounded-md mb-5"
+                  className="w-[150px] h-[150px] rounded-md mb-5"
                 />
-                <div className="m-2">
-                  <h1 className="font-medium px-3">{pic.product.name}</h1>
+
+                <div className="m-2 w-full">
+                  <h1 className="font-medium px-3 text-xl">{pic.product.title}</h1>
                   <div className="flex px-3 py-5 justify-start items-center">
                     <FaMinus
                       size={12}
                       className="cursor-pointer"
-                      onClick={() => 
+                      onClick={() =>
                         dispatch(decrementQuantity(pic.product.id))
                       }
                     />
@@ -56,18 +59,25 @@ const Items = () => {
                     size={15}
                     className="ml-2 cursor-pointer"
                     onClick={() => {
-                      dispatch(removeFromCart({productId:pic.product.id, price:pic.product.price}))
+                      dispatch(
+                        removeFromCart({
+                          productId: pic.product.id,
+                          price: pic.product.price,
+                        })
+                      );
                     }}
                   />
-                </div>
-                <div className="flex justify-end items-center">
-                  <h1 className="font-bold text-2xl">{Number(pic.product.price.toFixed(2))}{' '}$</h1>
+                  <div className="flex justify-end items-center">
+                    <h1 className="font-bold text-2xl">
+                      {Number(pic.product.price.toFixed(2))} $
+                    </h1>
+                  </div>
                 </div>
               </div>
             ))}
           </div>
-          <div>
-            Total Price : { totalQuantityPrice.toFixed(2) }
+          <div className="text-2xl font-bold bg-green-200 p-4 text-center">
+            Total Price : {totalQuantityPrice.toFixed(2)} $
           </div>
         </div>
       </div>
