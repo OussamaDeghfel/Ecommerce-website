6977dbdc6820762144e8f9ba245b036c63b955c9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-08 20:38:50 +0100

    implement the side bar items

--- src/components/accountDetails/accountDetails.tsx
+++ src/components/accountDetails/accountDetails.tsx
@@ -3,7 +3,7 @@ import SideBar from "./sideBar"
 const AccountDetails = () => {
   return (
     <div>
-        <h1 className='text-4xl font-bold'>
+        <h1 className='w-full h-full text-4xl font-bold border-2 border-black p-4'>
             <SideBar />
         </h1>
     </div>
--- src/components/accountDetails/sideBar.tsx
+++ src/components/accountDetails/sideBar.tsx
@@ -1,34 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const SideBar = () => {
   return (
     <div>
-        <ul>
-            <li>
-                Account Details
-            </li>
-            <li>
-            Security
-            </li>
-            <li>
-            Billing & Payment
-            </li>
-            <li>
-            Shipping Information
-            </li>
-            <li>
-            Order History
-            </li>
-            <li>
-            Wishlist/Favorites 
-            </li>
-            <li>
-            Account Deactivation or Deletion
-            </li>
-
-        </ul>
+      <ul className="flex flex-col bg-gray-50 h-fit w-[30vh] space-y-5">
+        <li className="text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          Account Details
+        </li>
+        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          Security
+        </li>
+        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          Billing & Payment
+        </li>
+        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          Order History
+        </li>
+        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          Wishlist/Favorites
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
