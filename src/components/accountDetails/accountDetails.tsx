75657457ade19dc26638a2a979ddd2c41c28c26a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-08 21:37:04 +0100

    add routes for the component

--- src/components/accountDetails/accountDetails.tsx
+++ src/components/accountDetails/accountDetails.tsx
@@ -1,10 +1,25 @@
+import { Route, Routes } from "react-router-dom"
 import SideBar from "./sideBar"
+import AccountInfo from "./detailsComponent/accountInfo"
+import FavorList from "./detailsComponent/favorList"
+import OrderHistory from "./detailsComponent/orderHistory"
+import Payment from "./detailsComponent/payment"
+import Security from "./detailsComponent/security"
 
 const AccountDetails = () => {
   return (
     <div>
-        <h1 className='w-full h-full text-4xl font-bold border-2 border-black p-4'>
+        <h1 className='w-full h-full flex text-4xl font-bold border-2 border-black p-4 space-x-4'>
             <SideBar />
+            <div className="border-2 border-gray-100 rounded-md p-5 w-full h-[80vh] shadow-inner">
+                <Routes>
+                    <Route path="/accountInfo" element={<AccountInfo />} />
+                    <Route path="/favorList" element={<FavorList />} />
+                    <Route path="/orderHistory" element={<OrderHistory />} />
+                    <Route path="/payment" element={<Payment />} />
+                    <Route path="/security" element={<Security />} />
+                </Routes>
+            </div>
         </h1>
     </div>
   )
