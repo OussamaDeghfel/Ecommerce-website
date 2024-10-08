9eddb93d7c861490220a23e1f6b5510b18f2f3b3
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-08 21:37:21 +0100

    reafctor the links

--- src/components/accountDetails/sideBar.tsx
+++ src/components/accountDetails/sideBar.tsx
@@ -4,21 +4,31 @@ const SideBar = () => {
   return (
     <div>
       <ul className="flex flex-col h-fit w-[35vh] space-y-5">
-        <li className="text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
-          <Link to="/accountInfo">Account Details</Link>
-        </li>
-        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
-          <Link to="/security">Security</Link>
-        </li>
-        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
-          <Link to="/payment">Billing & Payment</Link>
-        </li>
-        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
-          <Link to="/orderHistory">Order History</Link>
-        </li>
-        <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
-          <Link to="/favorList">Wishlist/Favorites</Link>
-        </li>
+        <Link to="accountInfo">
+          <li className="text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+            Account Details
+          </li>
+        </Link>
+        <Link to="security">
+          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+            Security
+          </li>
+        </Link>
+        <Link to="payment">
+          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+            Billing & Payment
+          </li>
+        </Link>
+        <Link to="orderHistory">
+          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+            Order History
+          </li>
+        </Link>
+        <Link to="favorList">
+          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+            Wishlist/Favorites
+          </li>
+        </Link>
       </ul>
     </div>
   );
