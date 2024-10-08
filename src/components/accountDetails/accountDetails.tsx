7a3fbe4ee51dca011bad10e667e26b691ddddd8b
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-08 16:10:40 +0100

    define accountDetails component

---
+++ src/components/accountDetails/accountDetails.tsx
@@ -0,0 +1,10 @@
+
+const accountDetails = () => {
+  return (
+    <div>
+        <h1 className='text-4xl font-bold'>Here you see the user account details </h1>
+    </div>
+  )
+}
+
+export default accountDetails
