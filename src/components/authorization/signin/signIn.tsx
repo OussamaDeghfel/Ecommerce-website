17dcad3e8c77429907628067866dfe91bb92cd21
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-09 14:43:08 +0100

    logic of signin

--- src/components/authorization/signin/signIn.tsx
+++ src/components/authorization/signin/signIn.tsx
@@ -1,12 +1,26 @@
 import { Button, Checkbox, Form, Input } from "antd";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
+
+type user = {
+  email: string;
+  password: string;
+}
 
 const SignIn = () => {
+
+
+  const verfiyCredential = (values : user) => {
+    if(values.email === localStorage.getItem("email") && values.password === localStorage.getItem("password")){
+      alert("Login Success")
+      redirect('/home')
+    } else {
+      alert ("wrong credential")
+    }
+  }
   return (
     <div className="flex w-full h-screen justify-center items-center">
       <div className="m-auto border-2 border-orange-100 rounded-md p-5 w-[400px] h-fit shadow-lg ">
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={verfiyCredential}>
           <Form.Item
             label="Email"
             name="email"
@@ -19,7 +33,7 @@ const SignIn = () => {
             name="password"
             rules={[{ required: true, message: "Please input your password!" }]}
           >
-            <Input />
+            <Input.Password />
           </Form.Item>
 
           <Form.Item name="remember" valuePropName="checked">
@@ -27,7 +41,7 @@ const SignIn = () => {
           </Form.Item>
 
           <Form.Item>
-            <Button type="primary" size="large">
+            <Button type="primary" size="large" htmlType="submit">
               Sign In
             </Button>
           </Form.Item>
