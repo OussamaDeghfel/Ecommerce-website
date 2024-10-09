4e5639e77d429373e0163fa72acee5441a945e9f
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-09 10:49:19 +0100

    change home path

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import About from "./components/about";
 import Home from "./components/home";
 import NavBar from "./components/navbar";
 import { Routes, Route } from "react-router-dom";
-import Contact from "./components/Contact";
 import StoreList from "./components/store/storeList";
 import { Provider } from "react-redux";
 import { store } from "./components/redux/store";
@@ -16,7 +15,7 @@ function App() {
       <NavBar />
       <div>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/*" element={<Home />} />
             <Route path="/store" element={<StoreList />} />
             <Route path="/about" element={<About />} />
             <Route path="/accountDetails/*" element={<AccountDetails />} />
