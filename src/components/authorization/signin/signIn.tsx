a86e66e5760e4af1a93251c1424e46e70a4a6ac6
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-09 12:09:03 +0100

    define the component elements

--- src/components/authorization/signin/signIn.tsx
+++ src/components/authorization/signin/signIn.tsx
@@ -1,9 +1,36 @@
+import { Checkbox, Form, Input } from 'antd'
 import React from 'react'
 
-const signIn = () => {
+const SignIn = () => {
   return (
-    <div>signIn</div>
+    <div>
+      <div>
+        <Form layout="vertical">
+          <Form.Item 
+          label="Email" 
+          name="email" 
+          rules={[{ required: true, message: 'Please input your email!' }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item 
+          label="Password" 
+          name="password" 
+          rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item 
+          name="remember"
+          valuePropName='checked'
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
 
-export default signIn
+export default SignIn
--- src/components/authorization/signup/signUp.tsx
+++ src/components/authorization/signup/signUp.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Form, Input } from "antd";
 
-const signUp = () => {
+const SignUp = () => {
   return (
-    <div>signUp</div>
-  )
-}
+    <div>
+      <div>
+        <Form layout="vertical">
+          <Form.Item
+            label="FirstName"
+            name="firstname"
+            rules={[
+              { required: true, message: "Please input your firstname!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="LastName"
+            name="lastname"
+            rules={[{ required: true, message: "Please input your lastname!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
 
-export default signUp
+export default SignUp;
