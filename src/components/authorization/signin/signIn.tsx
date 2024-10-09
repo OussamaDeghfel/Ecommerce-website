22ac895bb955ee7d02e80d559665c6c835de8705
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-09 15:06:00 +0100

    add incorrect credential warning

--- src/components/authorization/signin/signIn.tsx
+++ src/components/authorization/signin/signIn.tsx
@@ -1,4 +1,5 @@
 import { Button, Checkbox, Form, Input } from "antd";
+import { useState } from "react";
 import { Link, redirect } from "react-router-dom";
 
 type user = {
@@ -7,19 +8,28 @@ type user = {
 }
 
 const SignIn = () => {
+  const [credentialWarn, setCredentialWarn] = useState(false);
 
 
   const verfiyCredential = (values : user) => {
     if(values.email === localStorage.getItem("email") && values.password === localStorage.getItem("password")){
       alert("Login Success")
+      setCredentialWarn(false)
       redirect('/home')
     } else {
-      alert ("wrong credential")
+      setCredentialWarn(true)
     }
   }
   return (
     <div className="flex w-full h-screen justify-center items-center">
       <div className="m-auto border-2 border-orange-100 rounded-md p-5 w-[400px] h-fit shadow-lg ">
+        {credentialWarn && (
+          <div className="w-full h-fit p-4 bg-orange-200  text-center rounded-md mb-5">
+          <h1 className="font-bold text-red-600 ">Wrong Credentials</h1>
+          <p className="font-sm text-red-600">Invalid username or password</p>
+        </div>
+        )}
+        
         <Form layout="vertical" onFinish={verfiyCredential}>
           <Form.Item
             label="Email"
