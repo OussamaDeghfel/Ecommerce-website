cfb3ab6c4b7f9dfe55a9642af4a06b9d8f265899
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-12 14:39:24 +0100

    add the user login verification for the protected routing

--- src/utils/ProtectedRoutes.jsx
+++ src/utils/ProtectedRoutes.jsx
@@ -2,8 +2,9 @@ import { Outlet, Navigate } from "react-router-dom"
 
 const ProtectedRoutes = () => {
 
-    const user = null 
-    return user ? <Outlet /> : <Navigate to="/login" />
+    const user = localStorage.getItem("isLogin") 
+    user ? console.log("user is correct") : console.log("user is not correct")
+    return user ? <Outlet /> : <Navigate to="/" />
 }
 
 export default ProtectedRoutes
