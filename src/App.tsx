aa9c4ea8f41edf828c50d11948fe36bfb5e9ecbb
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-12 14:40:55 +0100

    refactor the routing between login and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/authorization/auth";
-// import ProtectedRoutes from "./utils/ProtectedRoutes";
-// import Home from "./components/dashboard/home";
-// import About from "./components/dashboard/about";
-// import StoreList from "./components/store/storeList";
-// import AccountDetails from "./components/dashboard/accountDetails/accountDetails";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import Dashboard from "./components/dashboard/dashboard";
 // import { Provider } from "react-redux";
 // import { store } from "./components/redux/store";
 // import NavBar from "./components/dashboard/navbar";
@@ -13,31 +10,14 @@ import Auth from "./components/authorization/auth";
 function App() {
   return (
     <>
-      {/* <Auth /> */}
-
       <div>
         <Routes>
           <Route path="/*" element={<Auth />} />
-          {/* <Route element={<ProtectedRoutes />}>
-            <Route path="/*" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/store" element={<StoreList />} />
-            <Route path="/accountDetails/*" element={<AccountDetails />} />
-          </Route> */}
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </div>
-
-      {/* <div>
-            <NavBar />
-        <Provider store={store}>
-          <Routes>
-            <Route path="/*" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/store" element={<StoreList />} />
-            <Route path="/accountDetails/*" element={<AccountDetails />} />
-          </Routes>
-        </Provider>
-      </div> */}
     </>
   );
 }
