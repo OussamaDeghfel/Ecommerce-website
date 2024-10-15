3eaba9f18a57a96d0280be52cccd91185bd2e87b
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-15 15:35:33 +0100

    fix the routing problem between the auth and the children after the login success

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/authorization/auth";
+// import Dashboard from "./components/dashboard/dashboard";
+// import SignIn from "./components/authorization/signin/signIn";
+// import SignUp from "./components/authorization/signup/signUp";
+import Home from "./components/dashboard/home";
+import About from "./components/dashboard/about";
+import StoreList from "./components/store/storeList";
+import AccountDetails from "./components/dashboard/accountDetails/accountDetails";
+import NavBar from "./components/dashboard/navbar";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
-import Dashboard from "./components/dashboard/dashboard";
-// import { Provider } from "react-redux";
-// import { store } from "./components/redux/store";
-// import NavBar from "./components/dashboard/navbar";
 
 function App() {
   return (
     <>
       <div>
         <Routes>
-          <Route path="/*" element={<Auth />} />
-          <Route element={<ProtectedRoutes />}>
-            <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Auth />} />
+          <Route element={<ProtectedRoutes children={undefined} />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/store" element={<StoreList />} />
+            <Route path="/accountDetails/*" element={<AccountDetails />} />
           </Route>
         </Routes>
       </div>
