b18a3d4c39cb1a6b33054fdf9b8ca493d17e45f8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-15 19:25:31 +0100

    refactor the styling of the details with the home image

--- src/components/dashboard/home/index.tsx
+++ src/components/dashboard/home/index.tsx
@@ -2,30 +2,32 @@ const Home = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center w-full">
-        <div className="flex w-full h-[90vh] bg-Banner rounded-lg bg-black/50">
-          <div className="flex w-full h-full bg-black/50 justify-start items-center rounded-md">
-            <h1 className="text-5xl text-white px-12">
-              <span className="text-orange-500 text-9xl font-bold">Welcome!</span> <br /> 
-              Discover exclusive deals today!
-            </h1>
-          </div>
-        </div>
-        <div className="grid grid-cols-4 gap-5">
-          <div className="w-24 h-50 bg-white rounded-md border-2 border-black">
-            <h2>900</h2>
-            <h1>Visit</h1>
-          </div>
-          <div className="w-24 h-50 bg-white rounded-md border-2 border-black">
-            <h2>12540 $</h2>
-            <h1>invest</h1>
-          </div>
-          <div className="w-24 h-50 bg-white rounded-md border-2 border-black">
-            <h2>450</h2>
-            <h1>Shipping</h1>
-          </div>
-          <div className="w-24 h-50 bg-white rounded-md border-2 border-black">
-            <h2>900</h2>
-            <h1>Visit</h1>
+        <div className="flex w-full h-[90vh] bg-Banner rounded-lg bg-black/60">
+          <div className="flex flex-col w-full h-full bg-black/40 justify-center items-center rounded-md">
+            <div className="text-5xl text-white p-8 text-center mt-12">
+              <h1 className="text-orange-500 text-9xl font-bold font-outline-4">
+                Welcome!
+              </h1>{" "}
+              <p className="font-outline-2"> Discover exclusive deals today!</p>
+            </div>
+            <div className="grid grid-cols-4 gap-5 mt-8">
+              <div className="w-40 h-50 bg-white rounded-md border-2 border-black p-5 text-center shadow-lg">
+                <h2>98%</h2>
+                <h1>Customer Satisfaction</h1>
+              </div>
+              <div className="w-40 h-50 bg-white rounded-md border-2 border-black p-5 text-center">
+                <h2>Shipping to 30+ </h2>
+                <h1>countries worldwide.</h1>
+              </div>
+              <div className="w-40 h-50 bg-white rounded-md border-2 border-black p-5 text-center">
+                <h2>450</h2>
+                <h1>Shipping</h1>
+              </div>
+              <div className="w-40 h-50 bg-white rounded-md border-2 border-black p-5 text-center">
+                <h2>+500</h2>
+                <h1>items sold this month!</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
