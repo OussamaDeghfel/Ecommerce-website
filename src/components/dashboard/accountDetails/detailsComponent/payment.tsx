290b8e01ee222fbc9f49a725d4377cc94fea1d73
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-20 18:16:04 +0100

    add the contact email to send the invoices

--- src/components/dashboard/accountDetails/detailsComponent/payment.tsx
+++ src/components/dashboard/accountDetails/detailsComponent/payment.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Checkbox, Input } from "antd";
+import React from "react";
 
 const Payment = () => {
   return (
-    <div>Payment</div>
-  )
-}
+    <div>
+      <div className="grid grid-cols-2 w-full h-full border-2 border-gray-300 rounded-md p-6">
+        <div className="flex flex-col">
+          <h1 className="text-xl ">Contact Email</h1>
+          <p className="text-sm text-gray-400">
+            Where should invoices be sent?
+          </p>
+        </div>
+        <div className="flex flex-col space-y-5 ">
+          <div className="flex flex-col">
+            <Checkbox className="">Send to my account email</Checkbox>
+            <span className="text-sm text-gray-400 px-6">
+              oussama@gmail.com
+            </span>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <Checkbox>Send to an alternative email</Checkbox>
+            <Input
+              placeholder="example@gmail.com"
+              className="w-fit mx-6"
+            />
+          </div>
+        </div>
+      </div>
 
-export default Payment
+      <div>Card details</div>
+      <div>Billing history</div>
+    </div>
+  );
+};
+
+export default Payment;
