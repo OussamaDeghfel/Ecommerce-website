c9fbbc97b09b66075403974c78daf88070642333
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-20 15:23:11 +0100

    add user Address info and logic of update, fix the typing problem of userInformation

--- src/components/dashboard/accountDetails/detailsComponent/accountInfo.tsx
+++ src/components/dashboard/accountDetails/detailsComponent/accountInfo.tsx
@@ -5,12 +5,20 @@ import { userSignUP } from "../../../authorization/signup/signUp";
 
 const AccountInfo = () => {
   const [userInformation, setUserInformation] = useState<userSignUP>({
-    firstname: localStorage.getItem("firstname"),
-    lastname: localStorage.getItem("lastname"),
-    email: localStorage.getItem("email"),
-  });
+    firstname: localStorage.getItem("firstname") as string,
+    lastname: localStorage.getItem("lastname") as string,
+    email: localStorage.getItem("email") as string,
+  } as userSignUP);
 
-  const [editable, setEditable] = useState(false);
+  const [userAddress, setUserAddress] = useState({
+    country: "Algeria",
+    city: "M'Sila",
+    stateLocation: "Cite 1",
+    postalCode: "5000",
+  })
+
+  const [infoEdit, setInfoEdit] = useState(false);
+  const [addressEdit, setAddressEdit] = useState(false);
 
   const handleChangeEditable = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -25,6 +33,11 @@ const AccountInfo = () => {
 
       return updateUserInformation;
     });
+
+    setUserAddress({
+      ...userAddress,
+      [e.target.name]: e.target.value,
+    })
   };
 
   return (
@@ -38,12 +51,14 @@ const AccountInfo = () => {
           <p className="text-base font-mono text-gray-500">Seller && Buyer</p>
         </div>
       </div>
+
+      {/* Personal information  */}
       <div className="flex flex-col border-2 border-gray-300 rounded-md p-4">
         <h1 className="text-xl font-bold pb-8">Personal Inforamtion</h1>
         <div className="flex justify-between px-10">
           <Form layout="vertical" className="grid grid-cols-2 gap-5">
             <Form.Item label="First Name">
-              {editable ? (
+              {infoEdit ? (
                 <Input
                   name="firstname"
                   value={userInformation.firstname}
@@ -56,7 +71,7 @@ const AccountInfo = () => {
               )}
             </Form.Item>
             <Form.Item label="Last Name">
-              {editable ? (
+              {infoEdit ? (
                 <Input
                   name="lastname"
                   value={userInformation.lastname}
@@ -69,7 +84,7 @@ const AccountInfo = () => {
               )}
             </Form.Item>
             <Form.Item label="Email">
-              {editable ? (
+              {infoEdit ? (
                 <Input
                   name="email"
                   value={userInformation.email}
@@ -81,10 +96,10 @@ const AccountInfo = () => {
             </Form.Item>
           </Form>
 
-          {editable ? (
+          {infoEdit ? (
             <Button
               className="w-fit font-bold"
-              onClick={() => setEditable(!editable)}
+              onClick={() => setInfoEdit(!infoEdit)}
               size="large"
             >
               Save
@@ -92,7 +107,7 @@ const AccountInfo = () => {
           ) : (
             <Button
               className="w-fit font-bold"
-              onClick={() => setEditable(!editable)}
+              onClick={() => setInfoEdit(!infoEdit)}
               size="large"
             >
               Edit
@@ -100,7 +115,81 @@ const AccountInfo = () => {
           )}
         </div>
       </div>
-      <div>address</div>
+
+      {/* Address information  */}
+      <div className="flex flex-col border-2 border-gray-300 rounded-md p-4">
+      <h1 className="text-xl font-bold pb-8">Address</h1>
+      <div className="flex justify-between px-10">
+          <Form layout="vertical" className="grid grid-cols-2 gap-x-24">
+            <Form.Item label="Country">
+              {addressEdit ? (
+                <Input
+                  name="country"
+                  value={userAddress.country}
+                  onChange={handleChangeEditable}
+                />
+              ) : (
+                <span className="text-gray-500">
+                  {userAddress.country}
+                </span>
+              )}
+            </Form.Item>
+            <Form.Item label="City">
+              {addressEdit ? (
+                <Input
+                  name="city"
+                  value={userAddress.city}
+                  onChange={handleChangeEditable}
+                />
+              ) : (
+                <span className="text-gray-500">
+                  {userAddress.city}
+                </span>
+              )}
+            </Form.Item>
+            <Form.Item label="State">
+              {addressEdit ? (
+                <Input
+                  name="email"
+                  value={userAddress.stateLocation}
+                  onChange={handleChangeEditable}
+                />
+              ) : (
+                <span className="text-gray-500">{userAddress.stateLocation}</span>
+              )}
+            </Form.Item>
+            <Form.Item label="Postal Code">
+              {addressEdit ? (
+                <Input
+                  name="postalCode"
+                  value={userAddress.postalCode}
+                  onChange={handleChangeEditable}
+                />
+              ) : (
+                <span className="text-gray-500">{userAddress.postalCode}</span>
+              )}
+            </Form.Item>
+          </Form>
+
+          {addressEdit ? (
+            <Button
+              className="w-fit font-bold"
+              onClick={() => setAddressEdit(!addressEdit)}
+              size="large"
+            >
+              Save
+            </Button>
+          ) : (
+            <Button
+              className="w-fit font-bold"
+              onClick={() => setAddressEdit(!addressEdit)}
+              size="large"
+            >
+              Edit
+            </Button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
