6a7a2c17756006c411d262d375534b8a3a1700dc
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-20 10:54:49 +0100

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/authorization/auth";
-// import Dashboard from "./components/dashboard/dashboard";
-// import SignIn from "./components/authorization/signin/signIn";
-// import SignUp from "./components/authorization/signup/signUp";
 import Home from "./components/dashboard/home";
 import About from "./components/dashboard/about";
 import StoreList from "./components/store/storeList";
 import AccountDetails from "./components/dashboard/accountDetails/accountDetails";
-import NavBar from "./components/dashboard/navbar";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 
 function App() {
