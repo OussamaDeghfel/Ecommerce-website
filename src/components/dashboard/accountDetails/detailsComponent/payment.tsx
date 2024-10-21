e5d6ef90304cb3dc34a4c30233ffd0313888a7c8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-21 11:53:52 +0100

    implement table contain billing history

--- src/components/dashboard/accountDetails/detailsComponent/payment.tsx
+++ src/components/dashboard/accountDetails/detailsComponent/payment.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Input } from "antd";
+import { Button, Checkbox, Input, Table } from "antd";
 import visaCard from "../../../../assets/visaCard.png";
 import masterCard from "../../../../assets/masterCard.png";
 import { BiPlusCircle } from "react-icons/bi";
@@ -7,6 +7,95 @@ import { useState } from "react";
 const Payment = () => {
 
   const [chooseDefaultCard, setChooseDefaultCard] = useState(0);
+
+  const columns = [
+    {
+      title: 'Invoice',
+      dataIndex: 'invoice',
+      key: 'invoice',
+    },
+    {
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    }
+  ];
+
+  const data = [
+    {
+      key: '1',
+      invoice: 'Invoice 1',
+      amount: '$100',
+      status: 'Pending',
+      date: '2024-10-10',
+    },
+    {
+      key: '2',
+      invoice: 'Invoice 2',
+      amount: '$200',
+      status: 'Paid',
+      date: '2024-06-18',
+    },
+    {
+      key: '3',
+      invoice: 'Invoice 3',
+      amount: '$300',
+      status: 'Pending',
+      date: '2023-10-12',
+    },
+    {
+      key: '4',
+      invoice: 'Invoice 4',
+      amount: '$400',
+      status: 'Paid',
+      date: '2024-07-24',
+    },
+    {
+      key: '5',
+      invoice: 'Invoice 5',
+      amount: '$500',
+      status: 'Pending',
+      date: '2024-09-29',
+    },
+    {
+      key: '6',
+      invoice: 'Invoice 6',
+      amount: '$600',
+      status: 'Paid',
+      date: '2024-10-01',
+    },
+    {
+      key: '7',
+      invoice: 'Invoice 7',
+      amount: '$700',
+      status: 'Pending',
+      date: '2024-05-16',
+    },
+    {
+      key: '8',
+      invoice: 'Invoice 8',
+      amount: '$800',
+      status: 'Paid',
+      date: '2024-08-17',
+    },
+    {
+      key: '9',
+      invoice: 'Invoice 9',
+      amount: '$900',
+      status: 'Pending',
+      date: '2024-02-24',
+    },
+  ];
 
   return (
     <div className="space-y-5">
@@ -78,7 +167,10 @@ const Payment = () => {
         </div>
       </div>
 
-      <div>Billing history</div>
+      <div className="flex flex-col space-y-5">
+      <h1 className="text-xl ">Billing History</h1>
+      <Table columns={columns} dataSource={data} />
+      </div>
     </div>
   );
 };
