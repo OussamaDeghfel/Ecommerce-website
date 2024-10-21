e53a4f2a2d01b1241e7f5715f1f08fc33bd1c2d6
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-21 12:00:22 +0100

    show the choosed detail section when click on it

--- src/components/dashboard/accountDetails/sideBar.tsx
+++ src/components/dashboard/accountDetails/sideBar.tsx
@@ -1,21 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const [choosedDetail, setChoosedDetail] = useState(0);
   return (
     <div>
       <ul className="flex flex-col h-fit w-[35vh] space-y-5">
         <Link to="accountInfo">
-          <li className="text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 1 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(1)}>
             Account Details
           </li>
         </Link>
         <Link to="security">
-          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 2 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(2)}>
             Security
           </li>
         </Link>
         <Link to="payment">
-          <li className="text-xl  p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300">
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 3 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(3)}>
             Billing & Payment
           </li>
         </Link>
