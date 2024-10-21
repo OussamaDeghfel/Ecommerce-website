0d47a2620744392d3472627b60ece84d581ae59a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-21 11:25:13 +0100

    implement the payment methods

--- src/components/dashboard/accountDetails/detailsComponent/payment.tsx
+++ src/components/dashboard/accountDetails/detailsComponent/payment.tsx
@@ -1,9 +1,11 @@
-import { Checkbox, Input } from "antd";
-import React from "react";
+import { Button, Checkbox, Input } from "antd";
+import visaCard from "../../../../assets/visaCard.png";
+import masterCard from "../../../../assets/masterCard.png";
+import { BiPlusCircle } from "react-icons/bi";
 
 const Payment = () => {
   return (
-    <div>
+    <div className="space-y-5">
       <div className="grid grid-cols-2 w-full h-full border-2 border-gray-300 rounded-md p-6">
         <div className="flex flex-col">
           <h1 className="text-xl ">Contact Email</h1>
@@ -20,15 +22,54 @@ const Payment = () => {
           </div>
           <div className="flex flex-col space-y-2">
             <Checkbox>Send to an alternative email</Checkbox>
-            <Input
-              placeholder="example@gmail.com"
-              className="w-fit mx-6"
-            />
+            <Input placeholder="example@gmail.com" className="w-fit mx-6" />
           </div>
         </div>
       </div>
 
-      <div>Card details</div>
+      <div className="grid grid-cols-2 w-full h-full border-2 border-gray-300 rounded-md p-6">
+        <div className="flex flex-col">
+          <h1 className="text-xl ">Card details</h1>
+          <p className="text-sm text-gray-400">Select default payment method</p>
+        </div>
+
+        <div className="grid grid-cols-1 gap-5">
+          <div className="flex h-full w-full border-2 border-blue-200 rounded-md p-4">
+            <div className="w-10">
+              <img src={visaCard} alt="visa card image" />
+            </div>
+            <div className="flex flex-col ml-5 space-y-1">
+              <h1 className="text-xl">Paysera Visa Card</h1>
+              <p className="text-sm text-gray-400">Expire on 22/09</p>
+              <div className="flex justify-start items-center space-x-5">
+                <Button className="text-base font-light">set as default</Button>
+                <Button>Edit</Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex h-full w-full border-2 border-blue-200 rounded-md p-4">
+            <div className="w-10">
+              <img src={masterCard} alt="visa card image" />
+            </div>
+            <div className="flex flex-col ml-5 space-y-1">
+              <h1 className="text-xl">Master Card</h1>
+              <p className="text-sm text-gray-400">Expire on 03/12</p>
+              <div className="flex justify-start items-center space-x-5">
+                <Button className="text-base font-light">set as default</Button>
+                <Button>Edit</Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex">
+            <Button>
+              <BiPlusCircle /> Add new card
+            </Button>
+          </div>
+        </div>
+      </div>
+
       <div>Billing history</div>
     </div>
   );
