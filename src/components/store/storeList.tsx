b02e4696029567ea82459a9bb6430f5d905c1d18
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-21 19:08:34 +0100

    add filter by category selection

--- src/components/store/storeList.tsx
+++ src/components/store/storeList.tsx
@@ -2,10 +2,12 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { appDispatch, RootState } from "../redux/store";
 import { fetchProducts } from "../redux/productSlice";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FaRegHeart, FaShoppingBag, FaStarHalfAlt } from "react-icons/fa";
 import { addToCart, chooseFavorite } from "../redux/cartSlice";
 import { FaStar, FaStarHalf } from "react-icons/fa6";
+import { FcClearFilters } from "react-icons/fc";
+import { Select } from "antd";
 // import { FcClearFilters } from "react-icons/fc";
 // import { BiHeart } from "react-icons/bi";
 
@@ -17,7 +19,7 @@ const StoreList = () => {
     setIsLiked(!isLiked);
   };
 
-  // const [searchByCategory, setSearchByCategory] = useState("");
+  const [searchByCategory, setSearchByCategory] = useState("");
 
   const dispatch = useDispatch<appDispatch>();
   const { product } = useSelector((state: RootState) => state.product);
@@ -26,71 +28,55 @@ const StoreList = () => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
-  console.log(product);
+  // console.log(product.map((item) => item.category));
+  console.log(product)
 
   //filter the category for only one name cause of duplicate
-  // const uniqueFilter = [
-  //   ...new Map(
-  //     product.map((item) => [item["category"]["name"], item])
-  //   ).values(),
-  // ];
+  const uniqueFilter = [
+    ...new Map(
+      product.map((item) => [item["category"], item])
+    ).values(),
+  ];
   // console.log("search by cat : ", searchByCategory);
 
-  // const filteredProduct = useMemo(() => {
-  //   if (searchByCategory) {
-  //     return product.filter((item) => item.category.name === searchByCategory);
-  //   } else {
-  //     return product;
-  //   }
-  // }, [searchByCategory, product]);
+  const filteredProduct = useMemo(() => {
+    if (searchByCategory) {
+      return product.filter((item) => item.category === searchByCategory);
+    } else {
+      return product;
+    }
+  }, [searchByCategory, product]);
 
   return (
     <div className="flex flex-col items-center justify-start h-full bg-gray-100 p-4">
       <h1 className="uppercase font-bold text-black text-2xl p-2 border-2 border-orange-600 rounded-lg ">
         Everything Comes in Your Mind
       </h1>
-      {/* <div className="flex justify-center items-center border-2 border-black">
+      <div className="flex justify-center items-center border-2 border-black">
         <div className="p-5 m-5 grid grid-cols-6 gap-4 w-full">
-          {uniqueFilter.map((prod) => (
-            <>
-              <button
-                value={prod.category.name}
-                onClick={() => setSearchByCategory(prod.category.name)}
-                className="px-4 py-2 bg-orange-500 text-white font-medium mx-2 rounded-md hover:scale-105 hover:duration-300"
-                key={prod.category.id}
-              >
-                {prod.category.name}
-              </button>
-            </>
-          ))}
-          <span>
-          <FcClearFilters
-            size={30}
-            onClick={() => setSearchByCategory("")}
-            className="cursor-pointer"
-          />
-        </span>
+          <Select className="w-full" placeholder="Filter By Category"
+          options={uniqueFilter.map((item) => ({value: item.category, label: item.category}))} onChange={(value) => setSearchByCategory(value)} allowClear  />
         </div>
-      </div> */}
+      </div>
       <div className="w-full h-full p-5 m-5 grid grid-cols-4 gap-6">
-        {product.map((prod) => (
+        {filteredProduct.map((prod) => (
           <>
             <div
               key={prod.id}
               className="w-full h-full bg-white pb-2 rounded-md shadow-md"
             >
-              <div className="relative">
+              <div className="relative shadow-md">
                 <img
-                  className="w-full h-64 rounded-t-md"
+                  className="w-full h-72 rounded-t-md"
                   src={prod.image}
                   alt="product image"
                 />
                 <div
-                  className=" absolute top-0 right-0 p-2 cursor-pointer hover:scale-125"
+                  className=" absolute top-0 right-0 p-2 cursor-pointer hover:scale-125 bg-gray-400 opacity-80 m-2 rounded-full"
                   onClick={FavToggle}
                 >
                   <FaRegHeart
-                    color="red"
+                  color="white"
                     size={25}
                     onClick={() => {
                       dispatch(
@@ -109,10 +95,10 @@ const StoreList = () => {
               <div className="flex flex-col p-2 justify-between h-[25.5vh]">
                 <div className="px-2">
                   <h1 className="font-bold text-md my-2">{prod.title}</h1>
-                  <h1 className="font-medium my-2">{prod.category}</h1>
+                  <h1 className="text-base my-2">{prod.category}</h1>
                 </div>
 
-                <div className="flex ">
+                <div className="flex font-bold">
                   {prod.rating.rate >= 4 && (
                     <div className="flex justify-center items-center space-x-4">
                       <FaStar size={20} className="mr-2" color="orange" />
