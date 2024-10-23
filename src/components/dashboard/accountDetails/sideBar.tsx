29e8127f52ce0d5eb427b7912970db75f30e47f9
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-23 15:34:33 +0100

    on the initial enter show the current tab active is accountDetails

--- src/components/dashboard/accountDetails/sideBar.tsx
+++ src/components/dashboard/accountDetails/sideBar.tsx
@@ -7,20 +7,20 @@ const SideBar = () => {
     <div>
       <ul className="flex flex-col h-fit w-[35vh] space-y-5">
         <Link to="accountInfo">
-          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 1 ? "bg-orange-100" : ""}`}
-          onClick={() => setChoosedDetail(1)}>
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 0 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(0)}>
             Account Details
           </li>
         </Link>
         <Link to="security">
-          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 2 ? "bg-orange-100" : ""}`}
-          onClick={() => setChoosedDetail(2)}>
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 1 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(1)}>
             Security
           </li>
         </Link>
         <Link to="payment">
-          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 3 ? "bg-orange-100" : ""}`}
-          onClick={() => setChoosedDetail(3)}>
+          <li className={`text-xl p-4 shadow-sm rounded-md cursor-pointer hover:scale-105 duration-300 ${choosedDetail === 2 ? "bg-orange-100" : ""}`}
+          onClick={() => setChoosedDetail(2)}>
             Billing & Payment
           </li>
         </Link>
