dbde2f1863fb10f8c9e6f4f86a8294189b50ee6d
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-23 13:58:25 +0100

    enh: reviews styling

--- src/components/dashboard/about/index.tsx
+++ src/components/dashboard/about/index.tsx
@@ -42,13 +42,13 @@ const About = () => {
     },
   ]
   return (
-    <div className="w-full h-full grid grid-cols-2 gap-4">
+    <div className="w-full h-full grid grid-cols-2 gap-4 mt-6">
       <div
-        className="w-full h-[80vh] flex flex-col bg-cover bg-center justify-center items-center text-center rounded-md"
+        className="w-full h-full flex flex-col bg-cover bg-center justify-center items-center text-center rounded-md"
         style={{ backgroundImage: `url(${customerRevImg})` }}
       >
         <div className="flex flex-col bg-white space-y-2 w-fit text-2xl uppercase p-4 rounded-md opacity-80">
-         <h1 className="font-semibold text-3xl"> Customers review about our store </h1>
+         <h1 className="font-semibold text-3xl"> Customers review </h1>
           <span className="font-bold text-5xl">2.6k+</span>
         </div>
 
@@ -56,7 +56,7 @@ const About = () => {
 
       <div className="flex flex-col w-full h-[80vh] rounded-md space-y-3 overflow-y-scroll">
         {costumerReviewsdata.map((item, index) => (
-          <div className="w-full h-fit flex flex-col p-5 rounded-lg space-y-4 bg-orange-50 border-2 border-orange-100" key={index}>
+          <div className="w-full h-fit flex flex-col p-5 rounded-lg space-y-4 bg-gray-100 border-2 border-gray-100 hover:bg-orange-400 duration-200 cursor-pointer hover:text-white" key={index}>
           <div className="text-start">
           "{item.review}"
           </div>
@@ -69,10 +69,7 @@ const About = () => {
           </div>
           </div>
         ))}
-        
       </div>
-
-      <div  className="w-full col-span-2 h-fit border-2 border-gray-400">invitation for try the store</div>
     </div>
   );
 };
