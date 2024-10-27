3e1f3001fef10825c0788c921642ba6aa742fe36
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-27 16:36:53 +0100

    implement the add new payment reducer

--- src/components/redux/paymentSlice.tsx
+++ src/components/redux/paymentSlice.tsx
@@ -1,33 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 interface PaymentCart {
+  cardId: number;
   cardName: string;
   cardNumber: string;
   expiryDate: string;
   cvv: string;
-  cardImg: string
+  cardImg: string;
 }
 
 interface paymentState {
   paymentMethods: PaymentCart[];
-} 
-
-const initialState: paymentState = {
-    paymentMethods: [
-        {
-            cardName: "",
-            cardNumber: "",
-            expiryDate: "",
-            cvv: "",
-            cardImg: ""
-        }
-    ]
 }
 
-const paymentSlice = createSlice({
-    name: "payment",
-    initialState,
-    reducers: {}
-})
+const initialState: paymentState = {
+  paymentMethods: [
+    // {
+    //     cardName: "",
+    //     cardNumber: "",
+    //     expiryDate: "",
+    //     cvv: "",
+    //     cardImg: ""
+    // }
+  ],
+};
 
-export default paymentSlice.reducer
+const paymentSlice = createSlice({
+  name: "payment",
+  initialState,
+  reducers: {
+    addNewPayment: (state, action) => {
+      state.paymentMethods.push(action.payload);
+    },
+  },
+});
+
+export default paymentSlice.reducer;
+export const { addNewPayment } = paymentSlice.actions;
