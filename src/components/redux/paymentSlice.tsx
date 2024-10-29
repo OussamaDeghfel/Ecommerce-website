620f458b590a18bed5cba0de102e0e782e191f56
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-10-29 15:21:19 +0100

    fix the deletion issue

--- src/components/redux/paymentSlice.tsx
+++ src/components/redux/paymentSlice.tsx
@@ -18,25 +18,33 @@ interface paymentState {
 const initialState: paymentState = {
   paymentMethods: [
     // {
-    //   cardId: 1,
-    //   cardName: "Visa",
-    //   cardNumber: "**** **** **** 1234",
-    //   expiryDate: "12/24",
-    //   cvv: "123",
-    //   cardImg: "/images/visa.png",
-    //   ChoosedAsDefault: false,
+    //   cardId: 12,
+    //   cardName: 'Credit bussiness 01',
+    //   expiryDate: 'Fri, 07 Mar 2025 23:00:00 GMT',
+    //   cardNumber: '2121515245121512',
+    //   cvv: '966',
+    //   cardImg: 'visacard',
+    //   ChoosedAsDefault: false
     // },
     // {
-    //   cardId: 2,
-    //   cardName: "MasterCard",
-    //   cardNumber: "**** **** **** 1234",
-    //   expiryDate: "12/24",
-    //   cvv: "123",
-    //   cardImg: "/images/masterCard.png",
-    //   ChoosedAsDefault: false,
+    //   cardId: 16,
+    //   cardName: 'debit card 01',
+    //   expiryDate: 'Thu, 13 Feb 2025 23:00:00 GMT',
+    //   cardNumber: '1512415215121213',
+    //   cvv: '548',
+    //   cardImg: 'visacard',
+    //   ChoosedAsDefault: false
+    // },
+    // {
+    //   cardId: 14,
+    //   cardName: 'Debit card 01',
+    //   expiryDate: 'Thu, 13 Mar 2025 23:00:00 GMT',
+    //   cardNumber: '6598986598659865',
+    //   cvv: '684',
+    //   cardImg: 'mastercard',
+    //   ChoosedAsDefault: false
     // }
   ],
-  //   paymentCardId: 0,
 };
 
 const paymentSlice = createSlice({
@@ -69,10 +77,10 @@ const paymentSlice = createSlice({
     },
     removePaymentMethod: (state, action) => {
       const findCard = state.paymentMethods.findIndex(
-        (item) => (item.cardId = action.payload)
-      );
+        (item) => item.cardId === action.payload
+      )
 
-      state.paymentMethods.splice(findCard, 1);
+      state.paymentMethods.splice(findCard, 1)
     },
   },
 });
