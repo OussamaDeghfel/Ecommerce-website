9df2a105caf2b8cda053a484b34f35d6300487a1
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-02 13:35:41 +0100

    fix the responsivity between the two navbar in different sizes

--- src/components/dashboard/accountDetails/accountDetails.tsx
+++ src/components/dashboard/accountDetails/accountDetails.tsx
@@ -1,24 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import SideBar from "./sideBar"
-import AccountInfo from "./detailsComponent/accountInfo"
-import Payment from "./detailsComponent/payment"
-import Security from "./detailsComponent/security"
+import { Route, Routes } from "react-router-dom";
+import SideBar from "./sideBar";
+import AccountInfo from "./detailsComponent/accountInfo";
+import Payment from "./detailsComponent/payment";
+import Security from "./detailsComponent/security";
 
 const AccountDetails = () => {
   return (
     <div>
-        <h1 className='w-full h-full flex text-4xl font-bold p-4 space-x-4'>
-            <SideBar />
-            <div className="border-2 border-gray-100 rounded-md p-5 w-full h-full shadow-inner">
-                <Routes>
-                    <Route path="/accountInfo" element={<AccountInfo />} />
-                    <Route path="/payment" element={<Payment />} />
-                    <Route path="/security" element={<Security />} />
-                </Routes>
-            </div>
-        </h1>
-    </div>
-  )
-}
+      <div className="w-full h-full flex flex-col md:flex-row sm:justify-center sm:items-center md:justify-start md:items-start text-4xl font-bold p-4 md:space-x-4">
+        <div>
+          <SideBar />
+        </div>
+        <div className="border-2 border-gray-100 rounded-md md:p-5 w-full h-full shadow-inner">
+          <Routes>
+            <Route path="/accountInfo" element={<AccountInfo />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/security" element={<Security />} />
+          </Routes>
+        </div>
 
-export default AccountDetails
+        
+      </div>
+    </div>
+  );
+};
+
+export default AccountDetails;
