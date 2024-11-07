15ff4dd4820c483d5f021b08f895f1c4c3c873c4
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-07 13:58:30 +0100

    fix the filtering issue when you first enter you found no Data

--- src/components/dashboard/store/storeList.tsx
+++ src/components/dashboard/store/storeList.tsx
@@ -2,50 +2,40 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { appDispatch, RootState } from "../../redux/store";
 import { favorite, fetchProducts } from "../../redux/productSlice";
-import { useEffect, useLayoutEffect, useState } from "react";
-// import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import { FaRegHeart, FaShoppingBag, FaStarHalfAlt } from "react-icons/fa";
 import {
   addToCart,
   chooseFavorite,
   removeFavorite,
 } from "../../redux/cartSlice";
-import { FaAngleDown, FaAngleUp, FaFilter, FaStar, FaStarHalf } from "react-icons/fa6";
+import {
+  FaAngleDown,
+  FaAngleUp,
+  FaFilter,
+  FaStar,
+  FaStarHalf,
+} from "react-icons/fa6";
 import { Button, Input, Select } from "antd";
-// import { FcClearFilters } from "react-icons/fc";
-// import { BiHeart } from "react-icons/bi";
+// import { ProductData } from "../../redux/types";
 
 const StoreList = () => {
-  // const [productList , setProductList] = useState([]);
   const dispatch = useDispatch<appDispatch>();
-  const  product  = useSelector((state: RootState) => state.product.product);
+  const product = useSelector((state: RootState) => state.product.product);
 
-  // const [prodFavorite, setProdFavorite] = useState<boolean | undefined>(Boolean);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [minPrice, setMinPrice] = useState("");
   const [maxPrice, setMaxPrice] = useState("");
   const [selectedRate, setSelectedRate] = useState(null);
-  const [filteredProduct, setFilteredProduct] = useState(product);
   const [favlist, setFavlist] = useState<string[]>([]);
   const [showFilter, setShowFiter] = useState(false);
+  const [filterActive, setFilterActive] = useState(false);
+  const [filteredProduct, setFilteredProduct] = useState(product);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     dispatch(fetchProducts());
-    setFilteredProduct(product);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[dispatch])
-
-  console.log("seeing the product : ", filteredProduct);
-
-  // useEffect(() => {
-  //   dispatch(fetchProducts());
-  //   setFilteredProduct(product);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [dispatch]);
-
-  // useMemo(() => {
-  //   setFilteredProduct(product);
-  // }, [product]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
 
   //filter the category for only one name cause of duplicate
   const uniqueFilter = [
@@ -53,38 +43,42 @@ const StoreList = () => {
   ];
 
   const handleSearch = () => {
+    setFilterActive(true);
     let filteredProduct = [...product];
 
-    if (selectedCategory) {
-      filteredProduct = filteredProduct.filter(
-        (item) => item.category === selectedCategory
-      );
-    }
-
-    if (minPrice) {
-      filteredProduct = filteredProduct.filter(
-        (item) => item.price >= Number(minPrice)
-      );
-    }
-    if (maxPrice) {
-      filteredProduct = filteredProduct.filter(
-        (item) => item.price <= Number(maxPrice)
-      );
-    }
-
-    if (selectedRate) {
-      if (selectedRate === "2") {
+    if (filterActive) {
+      if (selectedCategory) {
         filteredProduct = filteredProduct.filter(
-          (product) => product.rating.rate > 4.0
-        );
-      } else if (selectedRate === "3") {
-        filteredProduct = filteredProduct.filter(
-          (product) => product.rating.rate > 4.5
+          (item) => item.category === selectedCategory
         );
       }
-    }
 
-    setFilteredProduct(filteredProduct);
+      if (minPrice) {
+        filteredProduct = filteredProduct.filter(
+          (item) => item.price >= Number(minPrice)
+        );
+      }
+      if (maxPrice) {
+        filteredProduct = filteredProduct.filter(
+          (item) => item.price <= Number(maxPrice)
+        );
+      }
+
+      if (selectedRate) {
+        if (selectedRate === "2") {
+          filteredProduct = filteredProduct.filter(
+            (product) => product.rating.rate > 4.0
+          );
+        } else if (selectedRate === "3") {
+          filteredProduct = filteredProduct.filter(
+            (product) => product.rating.rate > 4.5
+          );
+        }
+      }
+
+      setFilteredProduct(filteredProduct);
+      console.log("Filtered Product :", filteredProduct);
+    }
   };
 
   const handleClear = () => {
@@ -93,6 +87,7 @@ const StoreList = () => {
     setMaxPrice("");
     setSelectedRate(null);
     setFilteredProduct(product);
+    setFilterActive(false);
   };
 
   const handleFavoriteProduct = (id: string) => {
@@ -109,15 +104,15 @@ const StoreList = () => {
 
   return (
     <div className="flex flex-col items-center justify-start h-full bg-gray-100 p-4">
-      <div className="flex md:hidden space-x-2 justify-center items-center" onClick={() => setShowFiter(!showFilter)}>
-        <FaFilter
-          size={25}
-          color="black"
-        />
+      <div
+        className="flex md:hidden space-x-2 justify-center items-center"
+        onClick={() => setShowFiter(!showFilter)}
+      >
+        <FaFilter size={25} color="black" />
         <span className="font-bold text-xl italic">FILTER</span>
-        {!showFilter ? <FaAngleDown size={15} /> : 
-        <FaAngleUp size={15} /> }
+        {!showFilter ? <FaAngleDown size={15} /> : <FaAngleUp size={15} />}
       </div>
+
       {showFilter && (
         <div className="flex justify-center items-center mt-2 md:hidden">
           <div className="p-2 grid grid-cols-1 gap-4 w-full rounded-md bg-gray-100 shadow-md ">
@@ -195,10 +190,7 @@ const StoreList = () => {
       <div className="hidden md:flex justify-center items-center">
         <div className="p-5 m-5 grid grid-cols-5 gap-6 w-full">
           <h1 className="uppercase flex font-medium items-center text-black text-xl italic w-fit">
-          <FaFilter
-          size={25}
-          color="black"
-        /> filter :
+            <FaFilter size={25} color="black" /> filter :
           </h1>
           <div className="flex flex-col pr-5 h-fit">
             <Select
@@ -271,8 +263,8 @@ const StoreList = () => {
       </div>
 
       <div className="w-full h-full p-10 m-5 grid md:grid-cols-3 lg:grid-cols-4 gap-8 border-t-2 border-gray-300">
-        {filteredProduct.map((prod) => (
-          <>
+        {!filterActive &&
+          product.map((prod) => (
             <div
               key={prod.id}
               className="w-full h-full bg-white pb-2 rounded-md shadow-md"
@@ -369,8 +361,107 @@ const StoreList = () => {
                 </div>
               </div>
             </div>
-          </>
-        ))}
+          ))}
+
+        {filterActive &&
+          filteredProduct.map((prod) => (
+            <div
+              key={prod.id}
+              className="w-full h-full bg-white pb-2 rounded-md shadow-md"
+            >
+              <div className="relative shadow-md">
+                <img
+                  className="w-full h-72 rounded-t-md"
+                  src={prod.image}
+                  alt="product image"
+                />
+                <div
+                  className={`absolute top-0 right-0 p-2 cursor-pointer hover:scale-125 m-2 rounded-full ${
+                    favlist.includes(prod.id)
+                      ? "bg-red-600 opacity-100"
+                      : "bg-gray-400 opacity-80"
+                  } `}
+                  onClick={() => {
+                    if (!favlist.includes(prod.id)) {
+                      dispatch(
+                        chooseFavorite({
+                          id: prod.id,
+                          title: prod.title,
+                          category: prod.category,
+                          image: prod.image,
+                          price: prod.price,
+                          rating: prod.rating,
+                        })
+                      ),
+                        dispatch(favorite(prod.id));
+                    } else if (favlist.includes(prod.id)) {
+                      dispatch(removeFavorite({ productID: prod.id }));
+                      dispatch(favorite(prod.id));
+                    }
+                    handleFavoriteProduct(prod.id);
+                  }}
+                >
+                  <FaRegHeart color="white" size={25} />
+                </div>
+              </div>
+
+              <div className="flex flex-col p-2 justify-between h-[30vh]">
+                <div className="px-2">
+                  <h1 className="font-bold text-md my-2">{prod.title}</h1>
+                  <h1 className="text-base my-2">{prod.category}</h1>
+                </div>
+
+                <div className="flex font-bold">
+                  {prod.rating.rate >= 4 && (
+                    <div className="flex justify-center items-center space-x-4">
+                      <FaStar size={20} className="mr-2" color="orange" />
+                      {prod.rating.rate}
+                    </div>
+                  )}
+                  {prod.rating.rate >= 3 && prod.rating.rate < 4 && (
+                    <>
+                      <FaStarHalfAlt
+                        size={20}
+                        className="mr-2"
+                        color="orange"
+                      />
+                      {prod.rating.rate}
+                    </>
+                  )}
+                  {prod.rating.rate < 3 && (
+                    <>
+                      <FaStarHalf size={20} className="mr-1" color="orange" />
+                      {prod.rating.rate}
+                    </>
+                  )}
+                  <span className="font-thin ml-2">({prod.rating.count})</span>
+                </div>
+
+                <div className="flex justify-between items-end ">
+                  <h1 className="font-bold my-2 text-xl">{prod.price} $</h1>
+                  <button
+                    className="flex items-center text-orange-600 font-bold p-2 border-2 border-orange-600 rounded-md  px-2 my-1 hover:bg-orange-500 hover:text-white duration-200"
+                    onClick={() => {
+                      dispatch(
+                        addToCart({
+                          product: {
+                            id: prod.id,
+                            title: prod.title,
+                            image: prod.image,
+                            price: prod.price,
+                          },
+                          quantity: 1,
+                          quantityPrice: prod.price,
+                        })
+                      );
+                    }}
+                  >
+                    <FaShoppingBag className="mr-2" /> Add To Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
